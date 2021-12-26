95bdefb5249ffde3ca5a9d0359f826bfb53853f2
Mohammed Shifreen <mshifreen@gmail.com>
2021-12-26 17:07:48 +0530

    Replaced accessing request Url from  to framework component

--- src/Infrastructure/Framework/Administration/Administration.php
+++ src/Infrastructure/Framework/Administration/Administration.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace OneCMS\Admin\Infrastructure\Framework\Administration;
 
-
 use OneCMS\Base\Application\Administration\AdministrationInterface;
 use OneCMS\Base\Application\Administration\AdministrationMenuInterface;
 use OneCMS\Base\Infrastructure\Framework\Web\Application\WebApplicationInterface;
@@ -14,7 +13,7 @@ use OneCMS\Base\Infrastructure\Framework\Web\Application\WebApplicationInterface
  * TODO: Add support to the sub-domain if possible. The administration currently supports only sub-path url pattern.
  *
  * @package getonecms/admin
- * @varsion 0.0.1
+ * @version 0.0.1
  * @since   0.0.1
  * @author  Mohammed Shifreen
  */
@@ -46,7 +45,7 @@ class Administration implements AdministrationInterface
         $this->menu = $menu;
         $this->path = $app->getConfig()->get('administrationPath') ?? $this->path;
         $this->url = $app->getComponent()->getUrlManager()->createAbsoluteUrl('/' . $this->path);
-        $requestedUrl = ltrim($_SERVER['REQUEST_URI'], '/');
+        $requestedUrl = ltrim($app->getComponent()->getRequest()->url, '/');
 
         if (strpos($requestedUrl, $this->path) !== false) {
             $this->inAdministration = true;
